794d908977f56826f6ccacfd98131a8939f5669c
fukuda01112 <fukuda-shota656@g.ecc.u-tokyo.ac.jp>
2017-10-29 16:39:39 +0900

    test

--- TopCoder/nbproject/Makefile-Release.mk
+++ TopCoder/nbproject/Makefile-Release.mk
@@ -36,6 +36,7 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 # Object Files
 OBJECTFILES= \
 	${OBJECTDIR}/Aaagmnrs.o \
+	${OBJECTDIR}/AlienAndHumbergers.o \
 	${OBJECTDIR}/main.o
 
 
@@ -67,6 +68,11 @@ ${OBJECTDIR}/Aaagmnrs.o: Aaagmnrs.cpp
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} "$@.d"
 	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Aaagmnrs.o Aaagmnrs.cpp
+
+${OBJECTDIR}/AlienAndHumbergers.o: AlienAndHumbergers.cpp
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/AlienAndHumbergers.o AlienAndHumbergers.cpp
 
 ${OBJECTDIR}/main.o: main.cpp
 	${MKDIR} -p ${OBJECTDIR}
