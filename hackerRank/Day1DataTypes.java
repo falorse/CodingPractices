bcd41008da446dec0ce87c375e01427d05577b8c
fukuda01112 <fukuda-shota656@g.ecc.u-tokyo.ac.jp>
2018-06-01 11:48:41 +0900

    date1 data types

---
+++ hackerRank/Day1DataTypes.java
@@ -0,0 +1,16 @@
+/* Declare second integer, double, and String variables. */
+
+/* Read and save an integer, double, and String to your variables.*/
+// Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+
+/* Print the sum of both integer variables on a new line. */
+
+/* Print the sum of the double variables on a new line. */
+
+/* Concatenate and print the String variables on a new line;
+  the 's' variable above should be printed first. */
+
+System.out.println(i + scan.nextInt());
+System.out.println(d + scan.nextDouble());
+String st = scan.nextLine();
+System.out.println(s + scan.nextLine());
